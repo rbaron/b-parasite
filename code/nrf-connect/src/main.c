9c9492567f48501e24abc952563818228eab1833
rbaron <rbaron@rbaron.net>
2022-11-13 11:20:31 +0100

    PWM kinda works

--- code/nrf-connect/src/main.c
+++ code/nrf-connect/src/main.c
@@ -1,4 +1,5 @@
 #include <drivers/i2c.h>
+#include <drivers/pwm.h>
 #include <logging/log.h>
 #include <zephyr/zephyr.h>
 
@@ -6,6 +7,16 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
+// PWM_DT_SPEC_GET(DT_LABEL(pwm0));
+static const struct pwm_dt_spec soil_pwm_dt =
+    PWM_DT_SPEC_GET(DT_NODELABEL(soil_pwm));
+static const uint32_t pulse = DT_PROP(DT_NODELABEL(soil_pwm), pulse);
+
 void main(void) {
-  prst_shtc3_read_t shtc3_read = prst_shtc3_read();
+  // prst_shtc3_read_t shtc3_read = prst_shtc3_read();
+  pwm_set_pulse_dt(&soil_pwm_dt, pulse);
+
+  while (true) {
+    k_msleep(500);
+  }
 }
---
+++ code/nrf-connect/src/prst/adc.c
@@ -0,0 +1,3 @@
+#include "adc.h"
+
+prst_adc_photo_sensor_t prst_adc_photo_read(double battery_voltage) {}
