3749d689bac5d7483f011a6645bb4ee6c3472e53
rbaron <rbaron@rbaron.net>
2022-11-16 00:00:16 +0100

    Very minimal, working BLE advertising

--- code/nrf-connect/src/main.c
+++ code/nrf-connect/src/main.c
@@ -1,4 +1,6 @@
 #include <logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/settings/settings.h>
 #include <zephyr/zephyr.h>
 
 #include "prst/adc.h"
@@ -8,6 +10,15 @@
 #include "prst/shtc3.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME),
+};
+
+static const struct bt_data sd[] = {
+    BT_DATA_BYTES(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME),
+};
 
 int main(void) {
   RET_IF_ERR(prst_adc_init());
@@ -19,6 +30,16 @@ int main(void) {
   prst_adc_read_t batt;
   prst_adc_soil_moisture_t soil;
   prst_adc_photo_sensor_t photo;
+
+  RET_IF_ERR(bt_enable(/*bt_reader_cb_t=*/NULL));
+
+  if (IS_ENABLED(CONFIG_SETTINGS)) {
+    RET_IF_ERR_MSG(settings_load(), "Error in settings_load()");
+  }
+
+  RET_IF_ERR(
+      bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd)));
+
   while (true) {
     RET_IF_ERR(prst_adc_batt_read(&batt));
     RET_IF_ERR(prst_adc_soil_read(batt.voltage, &soil));
@@ -28,7 +49,6 @@ int main(void) {
     LOG_INF("Soil: %.0f %% (%.3f mV)", 100 * soil.percentage,
             soil.adc_read.voltage);
     LOG_INF("Photo: %u lx (%.3f mV)", photo.brightness, soil.adc_read.voltage);
-
     k_msleep(500);
   }
 }
