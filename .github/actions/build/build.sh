4a6e3dcec307a79b1ad54c71e0cddab189859820
rbaron <rbaron@rbaron.net>
2022-11-28 19:44:41 +0100

    Update github action & workflow to build nrf-connect samples

--- .github/actions/build/build.sh
+++ .github/actions/build/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -eux -o pipefail
 
-export SDK_ROOT=/opt/nRF5_SDK
-export GNU_INSTALL_ROOT=/usr/bin/
+cd "${GITHUB_WORKSPACE}/${1}"
 
-cd "$GITHUB_WORKSPACE/code/b-parasite"
-make clean
-make lint
-make
+west build --build-dir ./build --pristine --board bparasite_nrf52840
