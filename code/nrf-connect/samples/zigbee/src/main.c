3b91d7525c22d12a339374af5b6bf613f6d6603e
Ole Odendahl <oleodendahl@gmail.com>
2022-12-16 12:15:36 +0100

    Improved basic attribute list to include more properties.

--- code/nrf-connect/samples/zigbee/src/main.c
+++ code/nrf-connect/samples/zigbee/src/main.c
@@ -57,13 +57,13 @@ ZB_ZCL_DECLARE_BASIC_ATTRIB_LIST_EXT(
     &dev_ctx.basic_attr.app_version,
     &dev_ctx.basic_attr.stack_version,
     &dev_ctx.basic_attr.hw_version,
-    dev_ctx.basic_attr.mf_name,
-    dev_ctx.basic_attr.model_id,
-    dev_ctx.basic_attr.date_code,
+    &dev_ctx.basic_attr.mf_name,
+    &dev_ctx.basic_attr.model_id,
+    &dev_ctx.basic_attr.date_code,
     &dev_ctx.basic_attr.power_source,
-    dev_ctx.basic_attr.location_id,
+    &dev_ctx.basic_attr.location_id,
     &dev_ctx.basic_attr.ph_env,
-    dev_ctx.basic_attr.sw_ver);
+    &dev_ctx.basic_attr.sw_ver);
 
 ZB_ZCL_DECLARE_TEMP_MEASUREMENT_ATTRIB_LIST(temp_measurement_attr_list,
                                             &dev_ctx.temp_measure_attrs.measure_value,
