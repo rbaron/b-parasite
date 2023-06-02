9285a6bc41c474ed9a113007541da31a83f39afc
Ole Odendahl <oleodendahl@gmail.com>
2023-06-02 11:15:46 +0200

    Added comment to describe nordic funny function name.

--- code/nrf-connect/samples/zigbee/src/restart_handler.c
+++ code/nrf-connect/samples/zigbee/src/restart_handler.c
@@ -8,6 +8,7 @@ LOG_MODULE_REGISTER(restart_handler, CONFIG_LOG_DEFAULT_LEVEL);
 
 static void restart_network_steering_cb(struct k_timer *timer) {
   LOG_DBG("Restart handler expired. Restarting network steering.");
+  // If the device is not commissioned, the rejoin procedure is started.
   user_input_indicate();
 }
 
