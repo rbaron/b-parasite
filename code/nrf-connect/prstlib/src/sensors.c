20d98d02a0c1c6bca6de6a95d1fea2cd28546247
Arne Seime <arne.seime@gmail.com>
2024-06-03 10:21:51 +0200

    Log photo adc value, not soil adc

--- code/nrf-connect/prstlib/src/sensors.c
+++ code/nrf-connect/prstlib/src/sensors.c
@@ -19,7 +19,7 @@ int prst_sensors_read_all(prst_sensors_t *sensors) {
   LOG_DBG("Soil: %.0f %% (%d mV)", 100 * sensors->soil.percentage,
           sensors->soil.adc_read.millivolts);
   LOG_DBG("Photo: %u lx (%d mV)", sensors->photo.brightness,
-          sensors->soil.adc_read.millivolts);
+          sensors->photo.adc_read.millivolts);
   LOG_DBG("Temp: %f oC", sensors->shtc3.temp_c);
   LOG_DBG("Humi: %.0f %%", 100 * sensors->shtc3.rel_humi);
   LOG_DBG("--------------------------------------------------");
