2beb2d4763f4afcad1f18291a64d1276e78627ee
Pferdebockwurst <37925404+Pferdebockwurst@users.noreply.github.com>
2025-10-30 15:14:29 +0100

    Update board.cmake to use nrfutil for flashing
    nrfjprog is deprecated and replaced by nrfutil

--- code/nrf-connect/prstlib/boards/bparasite/board.cmake
+++ code/nrf-connect/prstlib/boards/bparasite/board.cmake
@@ -9,7 +9,7 @@ board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
 endif()
 
 # set(OPENOCD_NRF5_SUBFAMILY "nrf52")
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
